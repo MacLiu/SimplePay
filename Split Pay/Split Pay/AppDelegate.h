c7705e91cc6da5d47a8df9895609e8ba822bfe74
Mac Liu <lium20@outlook.com>
2014-07-19 12:14:40 -0500

    Initial Commit

---
+++ Split Pay/Split Pay/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Split Pay
+//
+//  Created by Mac Liu on 7/19/14.
+//  Copyright (c) 2014 Mac Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Split Pay/Split Pay/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Split Pay
+//
+//  Created by Mac Liu on 7/19/14.
+//  Copyright (c) 2014 Mac Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
